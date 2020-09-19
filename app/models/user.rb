3190a6599c782c92dd51c52d3c61219cf61ea146
riirii <ryochan5630@yahoo.co.jp>
2020-09-19 18:58:54 +0900

    ログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,10 @@ class User < ApplicationRecord
   
   with_options presence: true do
     validates :nickname 
-    validates :last_name
-    validates :first_name
-    validates :last_name_kana
-    validates :first_name_kana 
+    validates :last_name,format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :first_name,format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :last_name_kana,format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_kana,format: { with: /\A[ァ-ヶー－]+\z/ } 
     validates :birthday
   end
 
