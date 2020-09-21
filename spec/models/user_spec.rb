51d0f343619583e759d4efb77555d6106d7d1932
riirii <ryochan5630@yahoo.co.jp>
2020-09-21 12:39:53 +0900

    プルリクエスト指摘内容修正２回目

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -55,6 +55,13 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include('Password is invalid')
     end
 
+    it 'passwordが半角英数字混合でなければ登録できないこと' do
+      @user.password = 'aaaaaa'
+      @user.password_confirmation = 'aaaaaa'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password is invalid')
+    end
+
     it 'passwordが存在してもpassword_confirmationが空では登録できないこと' do
       @user.password_confirmation = ''
       @user.valid?
