cdb29c2e4235a3a10dc01a5c2038abdddc2326b7
riirii <ryochan5630@yahoo.co.jp>
2020-09-24 19:35:31 +0900

    プルリクエスト指摘修正2回目

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,10 @@ Rails.application.routes.draw do
   root to: "items#index"
   devise_for :users, :controllers => {
     :registrations => 'users/registrations'
-    # :sessions => 'users/sessions'   
-  
   } 
 
     
-    resources :items, only: [:index, :new, :create]
+    resources :items, only: [:new, :create]
   
 
 
