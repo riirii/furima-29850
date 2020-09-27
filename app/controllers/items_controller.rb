a66bbd90f2b0b7c419fde26fb0458a2771267a44
riirii <ryochan5630@yahoo.co.jp>
2020-09-27 10:18:04 +0900

    RuboCop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,7 +35,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id]) 
+    item = Item.find(params[:id])
     if item.destroy
       redirect_to root_path
     else
