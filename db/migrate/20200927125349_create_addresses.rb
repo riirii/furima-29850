6ff9db33e950e3f9529ae8dda87097aae5edbb57
riirii <ryochan5630@yahoo.co.jp>
2020-10-03 11:24:37 +0900

    update

--- db/migrate/20200927125349_create_addresses.rb
+++ db/migrate/20200927125349_create_addresses.rb
@@ -2,7 +2,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
       t.string :postal_code,                null: false, default: ""
-      t.string :prefecture_id,             null: false
+      t.string :prefectures_id,             null: false
       t.string :municipalities,             null: false, default: ""
       t.string :address,                    null: false, default: ""
       t.string :building_name,              default: ""
--- db/migrate/20200928144943_rename_municipalities_column_to_addresses.rb
+++ db/migrate/20200928144943_rename_municipalities_column_to_addresses.rb
@@ -2,4 +2,5 @@ class RenameMunicipalitiesColumnToAddresses < ActiveRecord::Migration[6.0]
     def change
       rename_column :addresses, :municipalities, :municipality
     end
+    
 end
